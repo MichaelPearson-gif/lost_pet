c0d2e5c8f2639b50684f64abfed51ad40dcc104b
Michael Pearson <mpearson2516@yahoo.com>
2020-03-08 11:19:26 -0700

    Begin creating the lost pet form webpage.

--- Frontend/Pet_Locator/app.py
+++ Frontend/Pet_Locator/app.py
@@ -40,7 +40,6 @@ def home():
 @app.route("/lost_send", methods=["GET", "POST"])
 def Lsend():
     if request.method == "POST":
-        id = request.form["petID"]
         name = request.form["petName"]
         pet_type = request.form["petType"]
         age = request.form["petAge"]
@@ -61,7 +60,7 @@ def Lsend():
         return_state = request.form["returnState"]
         return_zip_code = request.form["returnZipCode"]
 
-        lost_pet = Lost(id=id, name=name, pet_type=pet_type, age=age, street_add=street_add, city=city, state=state, zip_code=zip_code, lat=lat, lng=lng, owner=owner, phone=phone, email=email, date=date, time=time, description=description, return_street_add=return_street_add, return_city=return_city, return_state=return_state, return_zip_code=return_zip_code)
+        lost_pet = Lost(name=name, pet_type=pet_type, age=age, street_add=street_add, city=city, state=state, zip_code=zip_code, lat=lat, lng=lng, owner=owner, phone=phone, email=email, date=date, time=time, description=description, return_street_add=return_street_add, return_city=return_city, return_state=return_state, return_zip_code=return_zip_code)
         db.session.add(lost_pet)
         db.session.commit()
         return redirect("/", code=302)
@@ -72,7 +71,6 @@ def Lsend():
 @app.route("/found_send", methods=["GET", "POST"])
 def Fsend():
     if request.method == "POST":
-        id = request.form["petID"]
         pet_type = request.form["petType"]
         age = request.form["petAge"]
         street_add = request.form["streetAddress"]
@@ -89,7 +87,7 @@ def Fsend():
         aquired = request.form["Aquired"]
         description = request.form["petDescription"]
 
-        found_pet = Found(id=id, pet_type=pet_type, age=age, street_add=street_add, city=city, state=state, zip_code=zip_code, lat=lat, lng=lng, founder=founder, phone=phone, email=email, date=date, time=time, aquired=aquired, description=description)
+        found_pet = Found(pet_type=pet_type, age=age, street_add=street_add, city=city, state=state, zip_code=zip_code, lat=lat, lng=lng, founder=founder, phone=phone, email=email, date=date, time=time, aquired=aquired, description=description)
         db.session.add(found_pet)
         db.session.commit()
         return redirect("/", code=302)
@@ -102,7 +100,6 @@ def lost_map():
 
     # List object to hold all the info I want to query from the DB
     sel = [
-        Lost.id,
         Lost.name,
         Lost.pet_type,
         Lost.age,
@@ -130,26 +127,25 @@ def lost_map():
     # Creating a dictionary to store the info from the db
     lost_pet = {}
     for result in results:
-        lost_pet["ID"] = result[0]
-        lost_pet["Pet Name"] = result[1]
-        lost_pet["Pet Type"] = result[2]
-        lost_pet["Pet Age"] = result[3]
-        lost_pet["Street Address"] = result[4]
-        lost_pet["City"] = result[5]
-        lost_pet["State"] = result[6]
-        lost_pet["Zip Code"] = result[7]
-        lost_pet["lat"] = result[8]
-        lost_pet["lng"] = result[9]
-        lost_pet["Owner's Name"] = result[10]
-        lost_pet["Owner's Phone Number"] = result[11]
-        lost_pet["Owner's Email"] = result[12]
-        lost_pet["Date"] = result[13]
-        lost_pet["Time"] = result[14]
-        lost_pet["Description"] = result[15]
-        lost_pet["Return Street Address"] = result[16]
-        lost_pet["Return City"] = result[17]
-        lost_pet["Return State"] = result[18]
-        lost_pet["Return Zip Code"] = result[19]
+        lost_pet["Pet Name"] = result[0]
+        lost_pet["Pet Type"] = result[1]
+        lost_pet["Pet Age"] = result[2]
+        lost_pet["Street Address"] = result[3]
+        lost_pet["City"] = result[4]
+        lost_pet["State"] = result[5]
+        lost_pet["Zip Code"] = result[6]
+        lost_pet["lat"] = result[7]
+        lost_pet["lng"] = result[8]
+        lost_pet["Owner's Name"] = result[9]
+        lost_pet["Owner's Phone Number"] = result[10]
+        lost_pet["Owner's Email"] = result[11]
+        lost_pet["Date"] = result[12]
+        lost_pet["Time"] = result[13]
+        lost_pet["Description"] = result[14]
+        lost_pet["Return Street Address"] = result[15]
+        lost_pet["Return City"] = result[16]
+        lost_pet["Return State"] = result[17]
+        lost_pet["Return Zip Code"] = result[18]
 
     # Returning the dictionary as a geojson objet
     return geojsonify(lost_pet)
@@ -160,7 +156,6 @@ def found_map():
 
     # List object to hold all the info I want to query from the DB
     sel = [
-        Found.id,
         Found.pet_type,
         Found.age,
         Found.street_add,
@@ -184,22 +179,21 @@ def found_map():
     # Creating a dictionary to store the info from the db
     found_pet = {}
     for result in results:
-        found_pet["ID"] = result[0]
-        found_pet["Pet Type"] = result[1]
-        found_pet["Age"] = result[2]
-        found_pet["Street Address"] = result[3]
-        found_pet["City"] = result[4]
-        found_pet["State"] = result[5]
-        found_pet["Zip Code"] = result[6]
-        found_pet["lat"] = result[7]
-        found_pet["lng"] = result[8]
-        found_pet["Founder's Name"] = result[9]
-        found_pet["Founder's Phone"] = result[10]
-        found_pet["Founder's Email"] = result[11]
-        found_pet["Date"] = result[12]
-        found_pet["Time"] = result[13]
-        found_pet["Aquired"] = result[14]
-        found_pet["Description"] = result[15]
+        found_pet["Pet Type"] = result[0]
+        found_pet["Age"] = result[1]
+        found_pet["Street Address"] = result[2]
+        found_pet["City"] = result[3]
+        found_pet["State"] = result[4]
+        found_pet["Zip Code"] = result[5]
+        found_pet["lat"] = result[6]
+        found_pet["lng"] = result[7]
+        found_pet["Founder's Name"] = result[8]
+        found_pet["Founder's Phone"] = result[9]
+        found_pet["Founder's Email"] = result[10]
+        found_pet["Date"] = result[11]
+        found_pet["Time"] = result[12]
+        found_pet["Aquired"] = result[13]
+        found_pet["Description"] = result[14]
 
     # Returning the dictionary as a geojson object
     return geojsonify(found_pet)
