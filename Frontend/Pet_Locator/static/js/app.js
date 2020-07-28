bc3dadbc9ca901216643d3ca1d197e1f44b741f7
Michael Pearson <mpearson2516@yahoo.com>
2020-07-27 21:17:34 -0700

    Re-added missing text

--- Frontend/Pet_Locator/static/js/app.js
+++ Frontend/Pet_Locator/static/js/app.js
@@ -1,7 +1,7 @@
 // Create the introduction text and append it to the <p> tag
 var intro = "Welcome to the Lost Pet Locator! Here you can report a lost pet or that you found a pet. You may even like to browse a geographical map to see all the reported lost pets in your neighborhood.";
 
-d3.select(".jumbotron").selectAll("p")
+d3.select(".header").selectAll("p")
     .text(intro)
     .enter()
     .html(function(d) {
@@ -11,7 +11,7 @@ d3.select(".jumbotron").selectAll("p")
 // Create a header for the h2 tag
 var choiceTitle = "What type of report would you like to file?";
 
-d3.select(".choice").selectAll("h2")
+d3.select("#report").selectAll("h2")
     .text(choiceTitle)
     .enter()
     .html(function(d) {
@@ -21,7 +21,7 @@ d3.select(".choice").selectAll("h2")
 // Create a header for map h2 tag
 var mapTitle = "Click on the button below to view all the reported lost/found pets";
 
-d3.select(".Map").selectAll("h2")
+d3.select("#Map").selectAll("h2")
     .text(mapTitle)
     .enter()
     .html(function(d) {
